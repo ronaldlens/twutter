2823bef4860e4ea23e360f246e5365814753274e
Ronald Lens <ronald@ronaldlens.com>
2012-09-05 22:55:13 +0200

    switched to normal routes and set root

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,48 +4,48 @@ describe "StaticPages" do
 
   describe "Home page" do
     it "should have the content 'Twutter'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_content('Twutter')
     end
 
     it "should have title 'Twutter'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('title', :text => 'Twutter')
     end
   end
 
   describe "Help page" do
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_content('Help')
     end
 
     it "should have the title 'Twutter | Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('title', :text => 'Twutter | Help')
     end      
   end
 
   describe "About page" do
     it "should have the content 'About'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_content('About')
     end
 
     it "should have the title 'Twutter | About'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('title', :text => 'Twutter | About')
     end
   end
 
   describe "Contact page" do
     it "should have the h1 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('h1', text: 'Contact')
     end
 
     it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('title', text: "Twutter | Contact")  
     end
   end
