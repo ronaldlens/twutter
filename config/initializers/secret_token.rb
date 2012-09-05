aed640ac697a41aa2bb785915285fd75afb6f95b
Ronald Lens <ronald@ronaldlens.com>
2012-09-05 09:42:03 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twutter::Application.config.secret_token = 'e6dcf1927b8180b523384e55e09c561a46d38bec9432642714061bb17a94b6bc3dee13db6162dced15f272504abf0433008d31ce8fc055e1ee1845ce86c013b4'
