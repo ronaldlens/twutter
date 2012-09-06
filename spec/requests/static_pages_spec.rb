e50643e049a488dffa70d9cc7edce33cb254fd3f
Ronald Lens <ronald@ronaldlens.com>
2012-09-06 08:47:42 +0200

    fixed footer misspelling, added full_title testing

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,11 +3,20 @@ require 'spec_helper'
 describe "StaticPages" do
 
   subject { page }
+
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading)}
+    it { should have_selector('title', text: full_title(page_title))}
+  end
+
   describe "Home page" do
     before { visit root_path }
 
-    it {should have_content('Twutter')}
-    it {should have_selector('title', text: full_title(''))}
+    let(:heading) {'Twutter'}
+    let(:page_title) {''}
+
+    it_should_behave_like "all static pages"
+    it { should_not have_selector 'title', text:'| Home'}
   end
 
   describe "Help page" do
@@ -29,6 +38,19 @@ describe "StaticPages" do
     it { should have_selector('h1', text: 'Contact')}
     it { should have_selector('title', text: full_title('Contact'))  }
   end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link 'About'
+    page.should have_selector 'title', text: full_title('About')
+    click_link 'Contact'
+    page.should have_selector 'title', text: full_title('Contact')
+    click_link 'Help'
+    page.should have_selector 'title', text: full_title('Help')
+    click_link 'Home'
+    page.should have_selector 'title', text: full_title('')
+  end
+
 
 
 end
