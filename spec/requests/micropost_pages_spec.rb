33f7dfd8645cb4c2e53f3be43d66dba4290d20fb
Ronald Lens <ronald@ronaldlens.com>
2012-09-10 22:32:47 +0200

    added delete micropost feature

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "MicropostPages" do
+describe "Micropost pages" do
   subject { page }
 
   let(:user) { FactoryGirl.create(:user)}
@@ -25,6 +25,18 @@ describe "MicropostPages" do
       before { fill_in 'micropost_content', with: "Lorem ipsum"}
       it "should create micropost" do
         expect { click_button "Post"}.to change(Micropost, :count)
+      end
+    end
+  end
+
+  describe "micropost destruction" do
+    before { FactoryGirl.create(:micropost, user: user)}
+
+    describe "as a correct user" do
+      before { visit root_path}
+
+      it "should delete a micropost" do 
+        expect { click_link "delete"}.to change(Micropost, :count).by(-1)
       end
     end
   end
